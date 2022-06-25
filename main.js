cc2dc6626b76cbbe4d5f6cfefc8bb44eea23bd35
Anderson B Almeida <andersonbalmeida@gmail.com>
2022-06-25 15:49:34 -0300

    quinto commit

--- main.js
+++ main.js
@@ -12,3 +12,21 @@
 
 getUser()
   
+
+    function Detalhes() {
+    axios.get(`https://upaa-55b4e-default-rtdb.firebaseio.com/pets/id1.json`)
+      .then(response => {
+        const data = response.data
+  
+        cor.textContent = data.cor
+        especie.textContent = data.especie
+        idade.textContent = data.idade
+        nome.textContent = data.nome
+        observacao.textContent = data.observacao
+        raca.textContent = data.raça
+        sexo.textContent = data.sexo
+      })
+      .catch(error => console.log(error))
+  }
+
+Detalhes()
